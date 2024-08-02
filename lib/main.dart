86ed28acec212427fd43fd6d4302fca709e342a3
Kasper Haverkamp <kasper.haverkamp@outlook.com>
2024-08-02 10:52:58 +0200

    Changed the state of the values to use the provider service instead of setstate

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:system_theme/system_theme.dart';
+import 'package:provider/provider.dart';
 
 void main() async{
   await SystemTheme.accentColor.load();
@@ -13,7 +14,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final accentColor = SystemTheme.accentColor.accent;
-    return MaterialApp(
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -35,10 +38,47 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'SevenUpSevenDown'),
+    ),
     );
   }
 }
 
+class MyAppState extends ChangeNotifier {
+  bool fightStage = false;
+  int playerTurn = 0;
+  int roundNumber = 0;
+  List<int> score = [0, 0, 0, 0];
+  List<int> expectedWins = [0, 0, 0, 0];
+  List<String> _names = ['Kasper', 'Erik', 'Claudia', 'Niek'];
+  int _counter = 0;
+
+  void _incrementCounter() {
+    _counter++;
+    notifyListeners();
+  }
+  void _subtractCounter() {
+    if (_counter > 0) {
+      _counter--;
+      notifyListeners();
+    }
+  }
+  void _undo() {
+    playerTurn--;
+    _counter = expectedWins[playerTurn];
+    notifyListeners();
+  }
+  void _submitExpectedWins() {
+    expectedWins[playerTurn] = _counter;
+    if (playerTurn < 3) {
+      playerTurn++;
+    } else {
+      playerTurn = 0;
+    }
+    _counter = 0;
+    notifyListeners();
+  }
+
+}
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -58,52 +98,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool fightStage = false;
-  int playerTurn = 0;
-  int roundNumber = 0;
-  int _counter = 0;
-  List<int> expectedWins = [0, 0, 0, 0];
-  List<int> score = [0, 0, 0, 0];
-  List<String> _names = ['Kasper', 'Erik', 'Claudia', 'Niek'];
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  var selectedIndex = 0;
 
-  void _subtractCounter() {
-    if (_counter > 0) {
-      setState(() {
-        _counter--;
-      });
-    }
-  }
-
-  void _submitExpectedWins() {
-      setState(() {
-        expectedWins[playerTurn] = _counter;
-        if (playerTurn < 3) {
-          playerTurn++;
-        } else {
-          playerTurn = 0;
-        }
-        _counter = 0;
-      });
-
-    }
-  void _undo() {
-    setState(() {
-      playerTurn--;
-      _counter = expectedWins[playerTurn];
-    });
-  }
   @override
   Widget build(BuildContext context) {
+    Widget page;
+
+    switch (selectedIndex) {
+      case 0:
+        page = ExpectedWinsPage();
+        break;
+      case 1:
+        page = Placeholder();
+        break;
+      default:
+        throw UnimplementedError('No widget for $selectedIndex');
+
+    }
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -121,65 +132,77 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body:
+      page
+    );
+  }
+}
 
-      Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Title(color: Colors.black, child: const Text('Round 1', style: TextStyle(fontSize:20, fontWeight: FontWeight.bold))),
-            Text('P1 ${expectedWins[0]} P2 ${expectedWins[1]} P3 ${expectedWins[2]} P3 ${expectedWins[3]}'),
-            ExpectedWinsCard(counter: _counter),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                FloatingActionButton.large(
-                    onPressed: _subtractCounter,
-                    child: const Icon(Icons.remove),
-                ),
-               const SizedBox(width: 20),
-               FloatingActionButton.large(
-                    onPressed: _incrementCounter,
-                    child: const Icon(Icons.add)
-                ),
-              ],
-            ),
-        SizedBox(height:30),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(onPressed: _undo, child: Text('undo turn')),
-            const SizedBox(width:20),
-            ElevatedButton(onPressed: _submitExpectedWins, child: Text('Submit')),
-          ],
-        ),
-            Row(
+class ExpectedWinsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var _names = appState._names;
+    var score = appState.score;
+    var expectedWins = appState.expectedWins;
+    var _counter = appState._counter;
+    return Center(
+      // Center is a layout widget. It takes a single child and positions it
+      // in the middle of the parent.
+      child: Column(
+        // Column is also a layout widget. It takes a list of children and
+        // arranges them vertically. By default, it sizes itself to fit its
+        // children horizontally, and tries to be as tall as its parent.
+        //
+        // Column has various properties to control how it sizes itself and
+        // how it positions its children. Here we use mainAxisAlignment to
+        // center the children vertically; the main axis here is the vertical
+        // axis because Columns are vertical (the cross axis would be
+        // horizontal).
+        //
+        // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+        // action in the IDE, or press "p" in the console), to see the
+        // wireframe for each widget.
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Title(color: Colors.black, child: const Text('Round 1', style: TextStyle(fontSize:20, fontWeight: FontWeight.bold))),
+          Text('P1 ${expectedWins[0]} P2 ${expectedWins[1]} P3 ${expectedWins[2]} P3 ${expectedWins[3]}'),
+          ExpectedWinsCard(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              FloatingActionButton.large(
+                onPressed: appState._subtractCounter,
+                child: const Icon(Icons.remove),
+              ),
+              const SizedBox(width: 20),
+              FloatingActionButton.large(
+                  onPressed: appState._incrementCounter,
+                  child: const Icon(Icons.add)
+              ),
+            ],
+          ),
+          const SizedBox(height:30),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(onPressed: appState._undo, child: Text('undo turn')),
+              const SizedBox(width:20),
+              ElevatedButton(onPressed: appState._submitExpectedWins, child: Text('Submit')),
+            ],
+          ),
+          Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Card(color:Colors.green, child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Column(
-                          children: [Text(_names[0]), Text('${score[0]}')]),
+                      children: [Text(_names[0]), Text('${score[0]}')]),
                 )
                 ),
                 Card(color:Colors.deepOrange, child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Column(
-                          children: [Text(_names[1]), Text('${score[1]}')]),
+                      children: [Text(_names[1]), Text('${score[1]}')]),
                 )
                 ),
                 Card(color:Colors.blue, child: Padding(
@@ -196,31 +219,24 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ]
 
-            )
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
 }
-
 class ExpectedWinsCard extends StatelessWidget {
-  const ExpectedWinsCard({
-    super.key,
-    required int counter,
-  }) : _counter = counter;
-
-  final int _counter;
 
   @override
   Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
     return SizedBox(
       width: 100,
       height: 100,
       child: Card(
         child: Center(
           child: Text(
-            '$_counter',
+            '${appState._counter}',
             style: Theme.of(context).textTheme.headlineMedium,
           ),
         ),
